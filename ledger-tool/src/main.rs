ed5d012feba04b3fbb0f90151f3e94f99647cb1a
Xiang Zhu <xzhu70@gmail.com>
2023-03-22 19:40:32 -0700

    Fix misc issues to get ledger-tool from_dir working

--- ledger-tool/src/main.rs
+++ ledger-tool/src/main.rs
@@ -104,7 +104,7 @@ use {
     std::{
         collections::{BTreeMap, BTreeSet, HashMap, HashSet},
         ffi::OsStr,
-        fs::File,
+        fs::{create_dir_all, File},
         io::{self, stdout, BufRead, BufReader, Write},
         path::{Path, PathBuf},
         process::{exit, Command, Stdio},
@@ -1204,6 +1204,13 @@ fn load_bank_forks(
         if path.exists() {
             move_and_async_delete_path(path);
         }
+        if !path.exists() {
+            // Keep the empty directory
+            create_dir_all(path).unwrap_or_else(|err| {
+                eprintln!("Error: {err:?}");
+                exit(1);
+            });
+        }
     });
     measure.stop();
     info!("done. {}", measure);
@@ -1465,7 +1472,7 @@ fn main() {
         .takes_value(false)
         .help("Do not start from a local snapshot if present");
     let snapshot_from_dir = Arg::with_name("snapshot_from_dir")
-        .long("snapshot-from-file")
+        .long("snapshot-from-dir")
         .takes_value(false)
         .help("Construct initial bank state from files instead of archives");
     let no_bpf_jit_arg = Arg::with_name("no_bpf_jit")
@@ -2949,7 +2956,7 @@ fn main() {
 
                 let blockstore = open_blockstore(
                     &ledger_path,
-                    AccessType::Secondary,
+                    AccessType::Primary,
                     wal_recovery_mode,
                     force_update_to_open,
                 );
