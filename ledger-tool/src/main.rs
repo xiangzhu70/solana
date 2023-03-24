189172ed0476d3bc7e6ac26237d15df58a47ce67
Xiang Zhu <xzhu70@gmail.com>
2023-03-24 10:17:48 -0700

    removed ledger-tool changes

--- ledger-tool/src/main.rs
+++ ledger-tool/src/main.rs
@@ -73,7 +73,7 @@ use {
         snapshot_minimizer::SnapshotMinimizer,
         snapshot_utils::{
             self, clean_orphaned_account_snapshot_dirs, create_all_accounts_run_and_snapshot_dirs,
-            move_and_async_delete_path, ArchiveFormat, SnapshotFrom, SnapshotVersion,
+            move_and_async_delete_path, ArchiveFormat, SnapshotVersion,
             DEFAULT_ARCHIVE_COMPRESSION, SUPPORTED_ARCHIVE_COMPRESSION,
         },
     },
@@ -104,7 +104,7 @@ use {
     std::{
         collections::{BTreeMap, BTreeSet, HashMap, HashSet},
         ffi::OsStr,
-        fs::{create_dir_all, File},
+        fs::File,
         io::{self, stdout, BufRead, BufReader, Write},
         path::{Path, PathBuf},
         process::{exit, Command, Stdio},
@@ -1114,15 +1114,10 @@ fn load_bank_forks(
             starting_slot = std::cmp::max(full_snapshot_slot, incremental_snapshot_slot);
         }
 
-        let snapshot_from = match arg_matches.is_present("snapshot_from_dir") {
-            true => SnapshotFrom::Dir,
-            false => SnapshotFrom::Archive,
-        };
         Some(SnapshotConfig {
             full_snapshot_archives_dir,
             incremental_snapshot_archives_dir,
             bank_snapshots_dir: bank_snapshots_dir.clone(),
-            snapshot_from,
             ..SnapshotConfig::new_load_only()
         })
     };
@@ -1203,17 +1198,6 @@ fn load_bank_forks(
     account_paths.iter().for_each(|path| {
         if path.exists() {
             move_and_async_delete_path(path);
-        }
-        if !path.exists() {
-            // Keep the empty directory
-            create_dir_all(path).unwrap_or_else(|err| {
-                eprintln!(
-                    "Failed to create accounts directory {}: {}",
-                    path.display(),
-                    err
-                );
-                exit(1);
-            });
         }
     });
     measure.stop();
@@ -1475,10 +1459,6 @@ fn main() {
         .long("no-snapshot")
         .takes_value(false)
         .help("Do not start from a local snapshot if present");
-    let snapshot_from_dir = Arg::with_name("snapshot_from_dir")
-        .long("snapshot-from-dir")
-        .takes_value(false)
-        .help("Construct initial bank state from files instead of archives");
     let no_bpf_jit_arg = Arg::with_name("no_bpf_jit")
         .long("no-bpf-jit")
         .takes_value(false)
@@ -1957,7 +1937,6 @@ fn main() {
             SubCommand::with_name("verify")
             .about("Verify the ledger")
             .arg(&no_snapshot_arg)
-            .arg(&snapshot_from_dir)
             .arg(&account_paths_arg)
             .arg(&accounts_index_path_arg)
             .arg(&halt_at_slot_arg)
@@ -2006,7 +1985,6 @@ fn main() {
             SubCommand::with_name("graph")
             .about("Create a Graphviz rendering of the ledger")
             .arg(&no_snapshot_arg)
-            .arg(&snapshot_from_dir)
             .arg(&account_paths_arg)
             .arg(&accounts_index_bins)
             .arg(&accounts_index_limit)
@@ -2041,7 +2019,6 @@ fn main() {
             SubCommand::with_name("create-snapshot")
             .about("Create a new ledger snapshot")
             .arg(&no_snapshot_arg)
-            .arg(&snapshot_from_dir)
             .arg(&account_paths_arg)
             .arg(&accounts_index_bins)
             .arg(&accounts_index_limit)
